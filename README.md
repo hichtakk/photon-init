f6f9f74835b2563f9c0f4304d6047e31a462faa0
Hirotaka Ichikawa <hichtakk@gmail.com>
2021-02-25 00:18:21 +0900

    update README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # photon-init
+
+```
+$ /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/hichtakk/photon-init/main/init.sh)"
+```
---
+++ init.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+# variables
+LINUX_STANDARD_BASE=${LINUX_STANDARD_BASE:-/etc/lsb-release}
+DEFAULT_USER=${DEFAULT_USER:-admin}
+
+# execution user validation
+if [ "$(whoami)" != "root" ]; then
+    echo "executed by non-root user"
+    exit 1
+fi
+
+# release validation
+if [ -e $LINUX_STANDARD_BASE ]; then
+    source $LINUX_STANDARD_BASE
+else
+    echo "lsb file not found"
+    exit 1
+fi
+if [ "${DISTRIB_ID}" != "VMware Photon OS" ]; then
+    echo "this is not Photon OS"
+    exit 1
+fi
+if [ "${DISTRIB_RELEASE}" != "4.0" ]; then
+    echo "'${DISTRIB_RELEASE}' is not supported Photon OS release version"
+    exit 1
+fi
+
+# add admin user
+useradd -m -G sudo,docker ${DEFAULT_USER}
+passwd ${DEFAULT_USER}
+
+# enable docker
+systemctl enable docker
+mkdir -p /etc/docker                                                                                                   
+cat <<EOF > /etc/docker/daemon.json
+{
+   "exec-opts": ["native.cgroupdriver=cgroupfs"],
+   "log-driver": "json-file",
+   "log-opts": {"max-size": "1m", "max-file": "3"}
+}
+EOF
+systemctl start docker
